b4c3d153f05907beed9266f28608adce9213868a
nehamanjunath08 <nehamanjunath08@gmail.com>
2025-07-07 20:24:06 +0530

    Basic Python function

--- day1_basics.py
+++ day1_basics.py
@@ -1,2 +1,5 @@
-print("Hello, World!")
-print("Hello, Neha")
+def greet(user):
+    print(f"Hello, {user}! Welcome to day 1.")      #f"..." is an f-string (formatted string literal) which allows you to insert variables directly inside the string using {}
+
+user = input("Enter your name:")
+greet(user)
